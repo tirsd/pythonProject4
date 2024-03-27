af80f9f788655a163b0c79855cae62d7df91cc40
Petrov.Ilya <tirsdionisa@mail.ru>
2024-03-27 09:50:17 +0300

    Add comps analyze. Rework yam,wb parser

--- 1.py
+++
@@ -1,6 +0,0 @@
-import pandas
-from sys import getdefaultencoding
-t = r'\\gold585.int\uk\Общее хранилище файлов\Служба аналитики\МЮР\tg_tn\.xlsx'.encode()
-t1  = t.decode('utf-8')
-p = pandas.read_excel(t1)
-print(p)
